1ca949b0451a2096e9956365697275f5d463d1d7
anthonysea <anthonyqchao@gmail.com>
2019-09-23 19:31:26 -0700

    Changed format of data from backend to account for future usage of fuzzy-search from fuse

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -3,9 +3,12 @@ from nba_api.stats.endpoints import commonallplayers
 def allplayers():
     players = commonallplayers.CommonAllPlayers(0, "00", "2018-19")
     resp = []
-    
+
     data = players.data_sets[0].get_dict()["data"]
     for entry in data:
-        resp.append([entry[0], entry[2]])
+        resp.append({
+            "name": entry[2],
+            "id": entry[0]
+        })
     print(resp)
     return resp, 200
